778e8fbad097c3adc7ec3da238528a76dd16d230
pjgorski <piotr@kod.com.pl>
2022-07-08 14:58:23 +0200

    new notebokks, plenty of result analysis

--- scripts/notebooks_netsense_funs.jl
+++ scripts/notebooks_netsense_funs.jl
@@ -746,3 +746,54 @@ function form_triad_network(all_triads, triad_nodes; all_triads_list = [])
 
     return As3
 end
+
+
+# dict should be create the following way. df has fields :a, :b, then:
+# a = [4] # where 4 is the value which extracted rows should have
+# b = [2]
+# dict = @dict a b
+function get_part_dataframe(df::DataFrame, dict::Dict; verbose = true)
+    cropped_res = deepcopy(df)
+    for (field, values) in dict
+        inds = findall(x->x in values, cropped_res[:, field])
+        cropped_res = cropped_res[inds,:]
+    end
+    if verbose
+        display("Extracted " * string(nrow(cropped_res)) * " rows.")
+    end
+    return cropped_res
+end
+
+
+function calc_error(b2b1, u2b1, b2b2, u2b2; method = "mse")
+
+    b2b_non_nan = (.!(isnan.(b2b1)) .+ .!(isnan.(b2b2)) ) .== 2
+
+    # if sum(b2b_non_nan) < length(b2b1)
+    #     return calc_error(b2b1[b2b_non_nan], u2b1, b2b2[b2b_non_nan], u2b2; method = method)
+    # end
+
+    u2b_non_nan = (.!(isnan.(u2b1)) .+ .!(isnan.(u2b2)) ) .== 2
+
+    # if sum(u2b_non_nan) < length(b2b1)
+    #     return calc_error(b2b1, u2b1[u2b_non_nan], b2b2, u2b2[u2b_non_nan]; method = method)
+    # end
+
+    if method == "mse"
+        error = sum((b2b1[b2b_non_nan] .- b2b2[b2b_non_nan]).^2) + sum((u2b1[u2b_non_nan] .- u2b2[u2b_non_nan]).^2)
+    elseif method == "mae"
+        error = sum(abs.(b2b1[b2b_non_nan] .- b2b2[b2b_non_nan])) + sum(abs.(u2b1[u2b_non_nan] .- u2b2[u2b_non_nan]))
+    elseif method == "mse_notbeg"
+        # b2b_non_nan[1:3] = 0
+        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mse")
+    elseif method == "mae_notbeg"
+        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mae")
+    end
+
+    return error
+end
+
+function show_res(df::DataFrame, dicts, num; mult = 5)
+    display(string(dicts[df[num, :dict_ind]]))
+    display(string(df[num, :time_ind]*mult))
+end
