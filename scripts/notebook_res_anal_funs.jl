759d690d9b01cecb654025c0ffbd51635726ee57
pjgorski <piotr@kod.com.pl>
2022-09-26 14:32:59 +0200

    new analysis

--- scripts/notebook_res_anal_funs.jl
+++ scripts/notebook_res_anal_funs.jl
@@ -28,7 +28,12 @@ function get_part_dataframe(df::DataFrame, dict::Dict; verbose = true)
 end
 
 
-function calc_error(b2b1, u2b1, b2b2, u2b2; method = "mse")
+function calc_error(b2b1, u2b1, b2b2, u2b2; method = "mse", b2b1_std = 0, u2b1_std = 0)
+
+    if b2b1_std == 0 || u2b1_std == 0
+        b2b1_std = zeros(size(b2b1))
+        u2b1_std = zeros(size(u2b1))
+    end
 
     b2b_non_nan = (.!(isnan.(b2b1)) .+ .!(isnan.(b2b2)) ) .== 2
 
@@ -48,20 +53,31 @@ function calc_error(b2b1, u2b1, b2b2, u2b2; method = "mse")
         error = sum(abs.(b2b1[b2b_non_nan] .- b2b2[b2b_non_nan])) + sum(abs.(u2b1[u2b_non_nan] .- u2b2[u2b_non_nan]))
     elseif method == "mse_notbeg"
         # b2b_non_nan[1:3] = 0
-        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mse")
+        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mse", b2b1_std = b2b1_std[4:end], u2b1_std = u2b1_std[4:end])
     elseif method == "mae_notbeg"
-        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mae")
+        return calc_error(b2b1[4:end], u2b1[4:end], b2b2[4:end], u2b2[4:end]; method = "mae", b2b1_std = b2b1_std[4:end], u2b1_std = u2b1_std[4:end])
     elseif method == "T--_mse_notbeg"
-        return calc_error(0, u2b1[4:end], 0, u2b2[4:end]; method = "mse")
+        return calc_error(0, u2b1[4:end], 0, u2b2[4:end]; method = "mse", b2b1_std = [0.], u2b1_std = u2b1_std[4:end])
     elseif method == "T--_mae_notbeg"
-        return calc_error(0, u2b1[4:end], 0, u2b2[4:end]; method = "mae")
+        return calc_error(0, u2b1[4:end], 0, u2b2[4:end]; method = "mae", b2b1_std = [0.], u2b1_std = u2b1_std[4:end])
     elseif method == "mse_manyT"
-        return calc_error(b2b1[4:end], u2b1[1:end-4], b2b2[4:end], u2b2[1:end-4]; method = "mse")
+        return calc_error(b2b1[4:end], u2b1[1:end-4], b2b2[4:end], u2b2[1:end-4]; method = "mse", b2b1_std = b2b1_std[4:end], u2b1_std = u2b1_std[1:end-4])
     elseif method == "mae_manyT"
-        return calc_error(b2b1[4:end], u2b1[1:end-4], b2b2[4:end], u2b2[1:end-4]; method = "mae")
+        return calc_error(b2b1[4:end], u2b1[1:end-4], b2b2[4:end], u2b2[1:end-4]; method = "mae", b2b1_std = b2b1_std[4:end], u2b1_std = u2b1_std[1:end-4])
     end
 
-    return error
+    if sum(b2b1_std) + sum(u2b1_std) == 0
+        return error
+    else
+        if method == "mse"
+            delta_error = sqrt(4 * sum(((b2b1[b2b_non_nan] .- b2b2[b2b_non_nan]) .* b2b1_std[b2b_non_nan]).^2) + 
+                4*sum(((u2b1[u2b_non_nan] .- u2b2[u2b_non_nan]) .* u2b1_std[u2b_non_nan]).^2) )
+        elseif method == "mae"
+            delta_error = sqrt(sum(( b2b1_std[b2b_non_nan]).^2) + sum((u2b1_std[u2b_non_nan]).^2) )
+        end
+        
+        return error, delta_error
+    end
 end
 
 function plot_dict(dicts, df, param_ind)
